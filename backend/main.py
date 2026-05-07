e52f2d45180d11871e782b0703c0e03d9fb51afe
khushal <khushalsatani009@gmail.com>
2026-05-07 23:41:04 +0530

    chore(backend): init backend

---
+++ backend/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from  src.router.upload_router import router 
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+    allow_credentials=True,
+)
+
+app.include_router( router, prefix="/api")
+  
